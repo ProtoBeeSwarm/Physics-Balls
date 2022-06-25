18b6090d97511849ece4e13d5c01c00222651a4e
NJCish <NJCish@gmail.com>
2022-06-25 13:48:02 -0600

    Update main to current (#3)
    * idk
    
    * Major restructuring:
    - Condensed to one file
    - Rewrite of like a *lot* of it
    - Seems to work better, fixes a few known bugs noteably:
      - Player still killed even when mouse not moved
      - No more self.self = self, That one line is a solid half of why I did a re-write :p
    
    Still to do:
    - Enemy-Enemy collisions are detected but nothing is done with them. Change this to a proper rebound, The trig shouldn't be too bad, and now I know how to do int(conditional) garbage which should be fun :p
    - Add a proper death screen, currently it just exits pygame
    
    * Adding sprites to BurningItAllDown branch
    
    * Adding Sprites to branch
    
    * Adding Sprites to branch
    
    * Deleting non-obsolete code from branch
    
    * Deleting non obsolete code from branch
    
    * Major restructuring:
    - Condensed to one file
    - Rewrite of like a *lot* of it
    - Seems to work better, fixes a few known bugs noteably:
      - Player still killed even when mouse not moved
      - No more self.self = self, That one line is a solid half of why I did a re-write :p
    
    Still to do:
    - Enemy-Enemy collisions are detected but nothing is done with them. Change this to a proper rebound, The trig shouldn't be too bad, and now I know how to do int(conditional) garbage which should be fun :p
    - Add a proper death screen, currently it just exits pygame
    
    - Moved png assets into a folder named 'Assets'

--- DepricatedCode.py
+++ DepricatedCode.py
@@ -34,3 +34,37 @@
     #     else:
     #         x_vel = (x_vel + ((-x_vel) * 2)) + random.randint(SPEED_MIN, SPEED_MAX)
     #return x_vel, y_vel
+
+# New colision code, needs to be fixed later
+# def collide(self, other, mask1, rect1, mask2, rect2):
+#     # self.vel
+#     # self.rect.center
+#     # other.vel
+#     # other.rect.center
+#     offset_x = rect2[0] - rect1[0]
+#     offset_y = rect2[1] - rect2[1]
+#     overlap = mask1.overlap(mask2, (offset_x, offset_y))
+#     if overlap:
+#         # Get vector magnitude (speed)
+#         ball_init_speed = self.vel.magnitude()
+#
+#         # Get angle of normal line
+#         ball_pos_delta = pygame.Vector2((self.vel.x-other.vel.x), (self.vel.y-other.vel.y))
+#         collision_normal_angle = math.atan(ball_pos_delta.y / ball_pos_delta.x)
+#         if ball_pos_delta.x < 0 and ball_pos_delta.y < 0:
+#             collision_normal_angle += math.pi
+#         # get initial angle of travel from vel vector
+#
+#         ball_init_travel_direc = math.atan(self.vel.y / self.vel.x)
+#         if self.vel.x < 0 and self.vel.y < 0:
+#             ball_init_travel_direc += math.pi
+#         # calculate difference in angle of normal line, and init travel direc
+#
+#         collision_delta_theta = collision_normal_angle - ball_init_travel_direc
+#         # mirror init trav direc over norm ln to get final travel direc angle
+#
+#         ball_final_travel_direc = ball_init_travel_direc + 2 * collision_delta_theta
+#         ball_final_travel_vec = pygame.Vector2(ball_init_speed * math.cos(ball_final_travel_direc),
+#                                                ball_init_speed * math.sin(ball_final_travel_direc))
+#         self.vel = ball_final_travel_vec  # overwrite self.vel, so it will now travel in the correct direc
+#         return self.vel
